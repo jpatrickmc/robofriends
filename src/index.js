2d39ad11c564b97b67dc46cf98efe386f397af23
Patrick McDaniel <dfwjpm@outlook.com>
2024-09-17 13:18:38 -0500

    add search

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from "react-dom/client"; //connects react to the browser DOM
 import "./index.css";
 import "tachyons"; //css library
 import reportWebVitals from "./reportWebVitals";
-import CardList from "./CardList";
-import { robots } from "./robots";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <CardList robots={robots} />
+    <App />
   </React.StrictMode>
 );
 
