9c4f684d45f3f4bcf3aa8eddc8b3d1bf07b71e3f
Patrick McDaniel <dfwjpm@outlook.com>
2024-09-17 16:58:44 -0500

    building react app 5

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
-import { robots } from "./robots";
 import SearchBox from "./components/SearchBox";
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: "",
     };
+    console.log("constructor");
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState({ robots: users });
+      });
   }
 
   onsSearchChange = (event) => {
@@ -22,13 +32,18 @@ class App extends Component {
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase());
     });
-    return (
-      <div className="tc">
-        <h1>RoboFriends</h1>
-        <SearchBox searchChange={this.onsSearchChange} />
-        <CardList robots={filteredRobots} />;
-      </div>
-    );
+    console.log("render");
+    if (this.state.robots.length === 0) {
+      return <h1 className="tc">Loading...</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1>RoboFriends</h1>
+          <SearchBox searchChange={this.onsSearchChange} />
+          <CardList robots={filteredRobots} />;
+        </div>
+      );
+    }
   }
 }
 
