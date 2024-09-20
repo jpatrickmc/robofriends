f50f6156939ceb27d01a228b03a8ea0e3302f6e6
Patrick McDaniel <dfwjpm@outlook.com>
2024-09-20 11:18:25 -0500

    add counter

--- src/containers/App.js
+++ src/containers/App.js
@@ -8,6 +8,7 @@ import "./App.css";
 function App() {
   const [robots, setRobots] = useState([]);
   const [searchfield, setSearchfield] = useState("");
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
     console.log("useEffect");
@@ -18,20 +19,28 @@ function App() {
       });
   }, []); // run only once
 
+  useEffect(() => {
+    console.log("useEffect", count);
+  }, [count]); // run when count changes
+
   const onsSearchChange = (event) => {
     setSearchfield(event.target.value);
+  };
+
+  const updateCounter = () => {
+    setCount(count + 1);
   };
 
   const filteredRobots = robots.filter((robot) => {
     return robot.name.toLowerCase().includes(searchfield.toLowerCase());
   });
-  console.log("render");
   if (!robots.length) {
     return <h1 className="tc">Loading...</h1>;
   } else {
     return (
       <div className="tc">
         <h1>RoboFriends</h1>
+        <button onClick={updateCounter}>Click Me</button>
         <SearchBox searchChange={onsSearchChange} />
         <Scroll>
           <ErrorBoundary>
